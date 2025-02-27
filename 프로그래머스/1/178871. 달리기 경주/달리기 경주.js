b0b3f7afc378d3d578f4ca8efe92c1b9cbff7e5d
HyeKyung Ku <rngprud123@naver.com>
2025-02-27 21:13:56 +0900

    [level 1] Title: 달리기 경주, Time: 191.85 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -3,14 +3,16 @@ function solution(players, callings) {
     let curName;
     let frontName;
     const idxList = {}
-
-    //{Alice: 0, Bob: 1, Charlie: 2}
+    
     players.forEach((name,index)=>idxList[name]=index)
     
     for(let call of callings){
+        
         idx = idxList[call]
+        
         curName = players[idx]
         frontName = players[idx-1]
+        
         idxList[call]-=1
         idxList[frontName]+=1
         players[idx] = frontName
