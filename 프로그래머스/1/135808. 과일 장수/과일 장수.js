5d89c20cb74f7fc7bc2564dc07958c73881cd8a0
HyeKyung Ku <rngprud123@naver.com>
2025-03-14 23:19:51 +0900

    [level 1] Title: 과일 장수, Time: 250.78 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,39 +1,39 @@
-function solution(k, m, score) {
-    let answer = 0;
-    score.sort((a, b) => b - a);
-    
-    for (let i = 0; i < score.length; i = i + m) {
-        const min = score[i + m - 1];
-        if (min) answer += min * m;
-    }
-    
-    return answer;
-}
-
-
 // function solution(k, m, score) {
-//     var answer = 0;
+//     let answer = 0;
+//     score.sort((a, b) => b - a);
     
-//     let sort = score.sort((a,b)=>b-a);
-//     let newArr = [];
-    
-//     for (let i = 0; i < score.length; i += m) {
-//         if(sort.slice(i, i + m).length === m){
-//             newArr.push(sort.slice(i, i + m));
-//         }
-//     }
-    
-//     for(let j = 0; j < newArr.length; j++){
-//         answer += Math.min(...newArr[j]) * m
-//         // console.log(Math.min(...newArr[j]))
-        
-//         // newArr.reduce((cur,acc)=>{
-//         //     Math.min(cur)
-//         // })
+//     for (let i = 0; i < score.length; i = i + m) {
+//         const min = score[i + m - 1];
+//         if (min) answer += min * m;
 //     }
     
 //     return answer;
 // }
+
+
+function solution(k, m, score) {
+    var answer = 0;
+    
+    let sort = score.sort((a,b)=>b-a);
+    let newArr = [];
+    
+    for (let i = 0; i < score.length; i += m) {
+        if(sort.slice(i, i + m).length === m){
+            newArr.push(sort.slice(i, i + m));
+        }
+    }
+    
+    for(let j = 0; j < newArr.length; j++){
+        answer += Math.min(...newArr[j]) * m
+        // console.log(Math.min(...newArr[j]))
+        
+        // newArr.reduce((cur,acc)=>{
+        //     Math.min(cur)
+        // })
+    }
+    
+    return answer;
+}
 
 // // 사과 점수 1 ~ k (k를 왜준거지? 필요없는거 아닌가)
 // // m개씩 묶음 [4,4,4], [4,4,4], [2,2,2], [2,1,1]
