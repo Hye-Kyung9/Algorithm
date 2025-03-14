f881dbff4c90caee7cd44bec24f60b20dbbe477e
HyeKyung Ku <rngprud123@naver.com>
2025-03-14 22:48:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 951.67 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,11 +1,10 @@
 function solution(elements) {
-    const n = elements.length;
     const extended = [...elements, ...elements]; // 원형 수열을 표현하기 위해 두 배로 확장 [7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
     const sums = new Set(); // 중복제거 객체
 
     // 부분 수열 길이 (1부터 n까지)
-    for (let length = 1; length <= n; length++) {
-        for (let start = 0; start < n; start++) { //시작 위치
+    for (let length = 1; length <= elements.length; length++) {
+        for (let start = 0; start < elements.length; start++) { //시작 위치
             let sum = 0;
 
             // length만큼 연속된 부분합 구하기
